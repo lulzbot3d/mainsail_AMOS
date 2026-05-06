cb7119222ed329a8a878590c3464e015c2d989e7
Carl Smith <csmith@lulzbot.com>
2026-05-06 11:57:02 -0500

    Restore speed factor control to show all the time. This was a Mini 3 "feature" that was carried over to the AMOS, but I don't like it.

--- src/components/panels/ToolheadControlPanel.vue
+++ src/components/panels/ToolheadControlPanel.vue
@@ -97,7 +97,7 @@
         </v-container>
         <!-- SPEED FACTOR -->
         <v-divider v-if="showSpeedFactor" />
-        <v-container v-if="printerIsPrintingOnly && showSpeedFactor">
+        <v-container v-if="showSpeedFactor">
             <tool-slider
                 :label="$t('Panels.ToolheadControlPanel.SpeedFactor')"
                 :icon="mdiSpeedometer"
